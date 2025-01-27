f9747986b90b597d131cc5083beaa049ed387eae
sinsoledad-cpp <3033056936@qq.com>
2025-01-28 01:17:25 +0800

    go-zero的rpc小demo

---
+++ src/github.com/fengfengzhidao/go_zero/_04rpc_study/user/rpc/internal/logic/userinfologic.go
@@ -0,0 +1,34 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+
+	"rpc/internal/svc"
+	"rpc/types/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserInfoLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfoLogic {
+	return &UserInfoLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *UserInfoLogic) UserInfo(in *user.UserInfoRequest) (*user.UserInfoResponse, error) {
+	// todo: add your logic here and delete this line
+	fmt.Println(in.UserId)
+	return &user.UserInfoResponse{
+		UserId:   in.UserId,
+		Username: "枫枫",
+	}, nil
+}
