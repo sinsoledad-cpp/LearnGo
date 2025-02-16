13df26a9137666caa740de4b397d16af0397cb07
sinsoledad-cpp <3033056936@qq.com>
2025-02-16 18:22:11 +0800

    .env加载,consul,models

--- src/github.com/CloudWeGo/gomall/app/user/main.go
+++ src/github.com/CloudWeGo/gomall/app/user/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"log"
 	"net"
 	"time"
 
+	"github.com/CloudWeGo/gomall/app/user/conf"
+	"github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/user/userservice"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	"github.com/joho/godotenv"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
-	"github.com/CloudWeGo/gomall/app/user/conf"
-	"github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/user/userservice"
+	consul "github.com/kitex-contrib/registry-consul"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func main() {
+	_ = godotenv.Load()
+
 	opts := kitexInit()
 
 	svr := userservice.NewServer(new(UserServiceImpl), opts...)
@@ -37,6 +42,13 @@ func kitexInit() (opts []server.Option) {
 	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
 		ServiceName: conf.GetConf().Kitex.Service,
 	}))
+
+	// consul
+	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	opts = append(opts, server.WithRegistry(r))
 
 	// klog
 	logger := kitexlogrus.NewLogger()
