42bb110dfb9eb60c90b72cd3f9327a06691cec19
sinsoledad-cpp <3033056936@qq.com>
2025-02-19 16:50:56 +0800

    cart相关后端接口代码

--- src/github.com/CloudWeGo/gomall/app/cart/biz/service/add_item.go
+++ src/github.com/CloudWeGo/gomall/app/cart/biz/service/add_item.go
@@ -2,7 +2,13 @@ package service
 
 import (
 	"context"
+
+	"github.com/CloudWeGo/gomall/app/cart/biz/dal/model"
+	"github.com/CloudWeGo/gomall/app/cart/biz/dal/mysql"
+	"github.com/CloudWeGo/gomall/app/cart/infra/rpc"
 	cart "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/cart"
+	"github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/product"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 )
 
 type AddItemService struct {
@@ -15,6 +21,24 @@ func NewAddItemService(ctx context.Context) *AddItemService {
 // Run create note info
 func (s *AddItemService) Run(req *cart.AddItemReq) (resp *cart.AddItemResp, err error) {
 	// Finish your business logic.
+	productResp, err := rpc.ProductClient.GetProduct(s.ctx, &product.GetProductReq{Id: req.Item.ProductId})
+	if err != nil {
+		return nil, err
+	}
+	if productResp == nil || productResp.Product.Id == 0 {
+		return nil, kerrors.NewBizStatusError(40004, "product not found")
+	}
 
-	return
+	cartItem := &model.Cart{
+		UserId:    req.UserId,
+		ProductId: req.Item.ProductId,
+		Qty:       req.Item.Quantity,
+	}
+
+	err = model.AddItem(s.ctx, mysql.DB, cartItem)
+	if err != nil {
+		return nil, kerrors.NewBizStatusError(50000, err.Error())
+	}
+
+	return &cart.AddItemResp{}, nil
 }
--- src/github.com/CloudWeGo/gomall/app/cart/biz/service/get_cart.go
+++ src/github.com/CloudWeGo/gomall/app/cart/biz/service/get_cart.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"context"
+
+	"github.com/CloudWeGo/gomall/app/cart/biz/dal/model"
+	"github.com/CloudWeGo/gomall/app/cart/biz/dal/mysql"
 	cart "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/cart"
+	"github.com/cloudwego/kitex/pkg/kerrors"
 )
 
 type GetCartService struct {
@@ -15,6 +19,16 @@ func NewGetCartService(ctx context.Context) *GetCartService {
 // Run create note info
 func (s *GetCartService) Run(req *cart.GetCartReq) (resp *cart.GetCartResp, err error) {
 	// Finish your business logic.
-
-	return
+	list, err := model.GetCartByUserId(s.ctx, mysql.DB, req.UserId)
+	if err != nil {
+		return nil, kerrors.NewBizStatusError(50002, err.Error())
+	}
+	var items []*cart.CartItem
+	for _, item := range list {
+		items = append(items, &cart.CartItem{
+			ProductId: item.ProductId,
+			Quantity:  item.Qty,
+		})
+	}
+	return &cart.GetCartResp{Items: items}, nil
 }
