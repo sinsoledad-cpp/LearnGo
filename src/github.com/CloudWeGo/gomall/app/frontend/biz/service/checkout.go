972f6bb25ece55a1190464091866a3dcbc2c8a8d
sinsoledad-cpp <3033056936@qq.com>
2025-02-23 22:07:51 +0800

    checkout前端页面,以及接口连接

--- src/github.com/CloudWeGo/gomall/app/frontend/biz/service/checkout.go
+++ src/github.com/CloudWeGo/gomall/app/frontend/biz/service/checkout.go
@@ -2,9 +2,15 @@ package service
 
 import (
 	"context"
+	"strconv"
 
 	common "github.com/CloudWeGo/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/CloudWeGo/gomall/app/frontend/infra/rpc"
+	frontendUtils "github.com/CloudWeGo/gomall/app/frontend/utils"
+	rpccart "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/cart"
+	rpcproduct "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/product"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 )
 
 type CheckoutService struct {
@@ -16,11 +22,44 @@ func NewCheckoutService(Context context.Context, RequestContext *app.RequestCont
 	return &CheckoutService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *CheckoutService) Run(req *common.Empty) (resp *common.Empty, err error) {
+func (h *CheckoutService) Run(req *common.Empty) (resp map[string]any, err error) {
 	//defer func() {
 	// hlog.CtxInfof(h.Context, "req = %+v", req)
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
-	return
+	var items []map[string]string
+	userId := frontendUtils.GetUserIdFromCtx(h.Context)
+
+	carts, err := rpc.CartClient.GetCart(h.Context, &rpccart.GetCartReq{UserId: uint32(userId)})
+	if err != nil {
+		return nil, err
+	}
+	var total float32
+
+	for _, v := range carts.Items {
+		productResp, err := rpc.ProductClient.GetProduct(h.Context, &rpcproduct.GetProductReq{
+			Id: v.ProductId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		if productResp.Product == nil {
+			continue
+		}
+		p := productResp.Product
+		items = append(items, map[string]string{
+			"Name":    p.Name,
+			"Price":   strconv.FormatFloat(float64(p.Price), 'f', 2, 64),
+			"Picture": p.Picture,
+			"Qty":     strconv.Itoa(int(v.Quantity)),
+		})
+		total += float32(v.Quantity) * p.Price
+	}
+
+	return utils.H{
+		"title": "Checkout",
+		"items": items,
+		"total": strconv.FormatFloat(float64(total), 'f', 2, 64),
+	}, nil
 }
--- src/github.com/CloudWeGo/gomall/app/frontend/biz/service/checkout_waiting.go
+++ src/github.com/CloudWeGo/gomall/app/frontend/biz/service/checkout_waiting.go
@@ -4,8 +4,12 @@ import (
 	"context"
 
 	checkout "github.com/CloudWeGo/gomall/app/frontend/hertz_gen/frontend/checkout"
-	common "github.com/CloudWeGo/gomall/app/frontend/hertz_gen/frontend/common"
+	"github.com/CloudWeGo/gomall/app/frontend/infra/rpc"
+	frontendUtils "github.com/CloudWeGo/gomall/app/frontend/utils"
+	rpccheckout "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/checkout"
+	rpcpayment "github.com/CloudWeGo/gomall/rpc_gen/kitex_gen/payment"
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 )
 
 type CheckoutWaitingService struct {
@@ -17,11 +21,37 @@ func NewCheckoutWaitingService(Context context.Context, RequestContext *app.Requ
 	return &CheckoutWaitingService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *CheckoutWaitingService) Run(req *checkout.CheckoutReq) (resp *common.Empty, err error) {
+func (h *CheckoutWaitingService) Run(req *checkout.CheckoutReq) (resp map[string]any, err error) {
 	//defer func() {
 	// hlog.CtxInfof(h.Context, "req = %+v", req)
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
-	return
+	userId := frontendUtils.GetUserIdFromCtx(h.Context)
+	_, err = rpc.CheckoutClient.Checkout(h.Context, &rpccheckout.CheckoutReq{
+		UserId:    uint32(userId),
+		Email:     req.Email,
+		Firstname: req.Firstname,
+		Lastname:  req.Lastname,
+		Address: &rpccheckout.Address{
+			Country:       req.Country,
+			ZipCode:       req.Zipcode,
+			City:          req.City,
+			State:         req.Province,
+			StreetAddress: req.Street,
+		},
+		CreditCard: &rpcpayment.CreditCardInfo{
+			CreditCardNumber:          req.CardNum,
+			CreditCardExpirationYear:  req.ExpirationYear,
+			CreditCardExpirationMonth: req.ExpirationMonth,
+			CreditCardCvv:             req.Cvv,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return utils.H{
+		"title":    "waiting",
+		"redirect": "/checkout/result",
+	}, nil
 }
