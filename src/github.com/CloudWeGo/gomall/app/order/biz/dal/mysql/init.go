4ce9de206d8e4f9663d57c1e59e66ed754c7ab21
sinsoledad-cpp <3033056936@qq.com>
2025-02-24 18:18:09 +0800

    consul,mysql基本配置

--- src/github.com/CloudWeGo/gomall/app/order/biz/dal/mysql/init.go
+++ src/github.com/CloudWeGo/gomall/app/order/biz/dal/mysql/init.go
@@ -1,7 +1,12 @@
 package mysql
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/CloudWeGo/gomall/app/order/biz/dal/model"
 	"github.com/CloudWeGo/gomall/app/order/conf"
+	"github.com/cloudwego/kitex/pkg/klog"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,7 +18,8 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -21,5 +27,10 @@ func Init() {
 	)
 	if err != nil {
 		panic(err)
+	}
+	if os.Getenv("GO_ENV") != "online" {
+		if err := DB.AutoMigrate(&model.Order{}, &model.OrderItem{}); err != nil {
+			klog.Error(err)
+		}
 	}
 }
